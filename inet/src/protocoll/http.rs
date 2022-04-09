02ce24ccf9f4b544d161106b8d5dbad17566a4eb
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2022-04-09 10:06:23 +0200

    fix: log request

--- inet/src/protocoll/http.rs
+++ inet/src/protocoll/http.rs
@@ -22,6 +22,7 @@ impl HttpReq {
 
 pub fn wrap_requests(buffer: &[u8]) -> HttpReq {
     let req = str::from_utf8(buffer).unwrap();
+    println!("Request: {}", req);
     let req = req.split(' ').collect::<Vec<&str>>();
     HttpReq {
         req_type: String::from(req[0]),
